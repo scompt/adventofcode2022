4ecb07ee348e2f5e174eedf068b7486cde2d949d
Edward Dale <scompt@scompt.com>
2022-12-03 11:05:21 +0100

    Day 02 Part 02

--- day02/main.go
+++ day02/main.go
@@ -15,20 +15,63 @@ type move struct {
 	oppo string
 	my   string
 }
+type move2 struct {
+	oppo    string
+	outcome string
+}
+
+func (m move2) score() int {
+	return m.scoreOutcome() + m.scoreMyMove()
+}
+func (m move2) scoreOutcome() int {
+	if m.outcome == "X" {
+		return 0
+	} else if m.outcome == "Y" {
+		return 3
+	} else if m.outcome == "Z" {
+		return 6
+	}
+	panic(m.outcome)
+}
+func (m move2) scoreMyMove() int {
+	var myShape string
+	if m.outcome == "Y" {
+		myShape = m.oppo
+	} else if m.outcome == "X" {
+		if m.oppo == "A" {
+			myShape = "C"
+		} else if m.oppo == "B" {
+			myShape = "A"
+		} else {
+			myShape = "B"
+		}
+	} else {
+		if m.oppo == "A" {
+			myShape = "B"
+		} else if m.oppo == "B" {
+			myShape = "C"
+		} else {
+			myShape = "A"
+		}
+	}
+
+	return scoreMyShape(myShape)
+}
 
 func (m move) score() int {
-	return m.scoreMove() + m.scoreMyShape()
+	return m.scoreMove() + scoreMyShape(m.my)
 }
-func (m move) scoreMyShape() int {
-	if m.my == "X" {
+func scoreMyShape(shape string) int {
+	if shape == "X" || shape == "A" {
 		return 1
-	} else if m.my == "Y" {
+	} else if shape == "Y" || shape == "B" {
 		return 2
-	} else if m.my == "Z" {
+	} else if shape == "Z" || shape == "C" {
 		return 3
 	}
-	panic(m.my)
+	panic(shape)
 }
+
 func (m move) scoreMove() int {
 	if (m.oppo == "A" && m.my == "X") ||
 		(m.oppo == "B" && m.my == "Y") ||
@@ -43,7 +86,16 @@ func (m move) scoreMove() int {
 	}
 }
 
-func scoreMoves(moves []move) []int {
+func scoreMoves1(moves []move) []int {
+	scores := make([]int, len(moves))
+
+	for i, m := range moves {
+		scores[i] = m.score()
+	}
+
+	return scores
+}
+func scoreMoves2(moves []move2) []int {
 	scores := make([]int, len(moves))
 
 	for i, m := range moves {
@@ -53,7 +105,7 @@ func scoreMoves(moves []move) []int {
 	return scores
 }
 
-func parseInput(input string) []move {
+func parseInput1(input string) []move {
 	lines := strings.Split(input, "\n")
 	var moves []move
 
@@ -69,15 +121,41 @@ func parseInput(input string) []move {
 
 	return moves
 }
+func parseInput2(input string) []move2 {
+	lines := strings.Split(input, "\n")
+	var moves []move2
+
+	for _, line := range lines {
+		if len(line) < 3 {
+			break
+		}
+
+		oppoMove := string(line[0])
+		outcome := string(line[2])
+		moves = append(moves, move2{oppoMove, outcome})
+	}
+
+	return moves
+}
 
 func partOne(input string) int {
-	moves := parseInput(input)
-	scores := scoreMoves(moves)
+	moves := parseInput1(input)
+	scores := scoreMoves1(moves)
+	// fmt.Printf("%v\n", scores)
+	sum := util.SumIntSlice(scores)
+	return sum
+}
+func partTwo(input string) int {
+	moves := parseInput2(input)
+	scores := scoreMoves2(moves)
 	// fmt.Printf("%v\n", scores)
 	sum := util.SumIntSlice(scores)
 	return sum
 }
 func main() {
-	score := partOne(input2)
-	fmt.Printf("%d\n", score)
+	score1 := partOne(input2)
+	fmt.Printf("%d\n", score1)
+
+	score2 := partTwo(input2)
+	fmt.Printf("%d\n", score2)
 }
--- day02/main_test.go
+++ day02/main_test.go
@@ -11,7 +11,12 @@ import (
 var input1 string
 
 func TestOne(t *testing.T) {
-	assert.Equal(t, 3, len(parseInput(input1)), "they should be equal")
+	assert.Equal(t, 3, len(parseInput1(input1)), "they should be equal")
 	assert.Equal(t, 15, partOne(input1), "they should be equal")
+	// assert.Equal(t, 66616, partOne(input2), "they should be equal")
+}
+func TestTwo(t *testing.T) {
+	assert.Equal(t, 3, len(parseInput2(input1)), "they should be equal")
+	assert.Equal(t, 12, partTwo(input1), "they should be equal")
 	// assert.Equal(t, 66616, partOne(input2), "they should be equal")
 }
